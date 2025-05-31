cdd8a531f03c343e62b42e1cbbf99172fea87735
NebulaScout <ribak427@gmail.com>
2025-05-31 07:25:18 +0300

    Add shadcn dependencies into the project

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -5,7 +5,7 @@ export default function layout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <main className="">
+    <main className="font-work-sans">
       <Navbar />
       {children}
     </main>
