9bb01be693b267c4bfc3ba12255c7545e9429b1a
NebulaScout <ribak427@gmail.com>
2025-06-23 10:24:12 +0300

    Fetch data and add type safety

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,17 +1,7 @@
 import SearchForm from "@/components/SearchForm";
-import StartupCard from "@/components/StartupCard";
-// import { title } from "process";
-
-// type StartupCardType = {
-//   _createdAt: string;
-//   views: number;
-//   author: { _id: number };
-//   _id: number;
-//   description: string;
-//   image: string;
-//   category: string;
-//   title: string;
-// };
+import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
+import { client } from "@/sanity/lib/client";
+import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 
 export default async function Home({
   searchParams,
@@ -20,19 +10,7 @@ export default async function Home({
 }) {
   const query = (await searchParams).query;
 
-  const posts = [
-    {
-      _createdAt: new Date(),
-      views: 55,
-      author: { _id: 1, name: "John Doe" },
-      _id: 1,
-      description: "This is a description.",
-      image:
-        "https://unsplash.com/photos/person-in-orange-and-white-robot-costume-eZWGK5sIiBM",
-      category: "Robots",
-      title: "We Robots",
-    },
-  ];
+  const posts = await client.fetch(STARTUPS_QUERY);
 
   return (
     <>
@@ -54,7 +32,7 @@ export default async function Home({
 
         <ul className="mt-7 card_grid">
           {posts?.length > 0 ? (
-            posts.map((post: StartupCardType, index: number) => (
+            posts.map((post: StartupTypeCard) => (
               <StartupCard key={post?._id} post={post} />
             ))
           ) : (
